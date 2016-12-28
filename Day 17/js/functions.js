6e5f2c2fb343b3eb9f0a007e633c604b50146333
MarcusGitAccount <pop_marcus_98@yahoo.com>
2016-12-28 18:53:18 +0200

    day 17 sorting without article

---
+++ Day 17/js/functions.js
@@ -0,0 +1,21 @@
+
+let bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const articleMatch = new RegExp(/^(The |A |An )/);
+
+bands.sort((a, b) => {
+  let articleA = a.match(articleMatch) || '';
+  let articleB = b.match(articleMatch) || '';
+
+  a = a.replace(articleA[0], '');
+  b = b.replace(articleB[0], '');
+
+  if (a.toLowerCase() > b.toLowerCase())
+    return 1;
+  else if (a.toLowerCase() === b.toLowerCase())
+    return 0;
+  else 
+    return -1;
+});
+
+document.querySelector('#result-ul').innerHTML = bands.map(band => `<li>${band}</li>`).join('');
