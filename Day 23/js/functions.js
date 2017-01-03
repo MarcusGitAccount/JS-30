1f72ec07fb7314515ca792f2a4709dbe623512a2
MarcusGitAccount <pop_marcus_98@yahoo.com>
2017-01-03 19:15:03 +0200

    day 23

--- Day 23/js/functions.js
+++ Day 23/js/functions.js
@@ -2,29 +2,52 @@
 if (!('speechSynthesis' in window))
   alert('Your browser does not support speechSynthesis');
 
-let isClosed = 0;
+let timer = null;
+let voices = [];
 let inputRanges = document.querySelectorAll('input[type=range]');
 let dropdown = document.querySelector('select[name=voices]');
+let buttons = document.querySelectorAll('input[type=button]');
+let textarea = document.querySelector('textarea');
 let speech = new SpeechSynthesisUtterance();
-let voices = Array.from(window.speechSynthesis.getVoices());
 let str = "The Web Speech API adds voice recognition (speech to text) and speech synthesis (text to speech) to JavaScript. The post briefly covers the latter, as the API recently landed in Chrome 33 (mobile and desktop).";
 
-function changeVoice(e){ 
-  let options = speechSynthesis.getVoices().map(voice => `<option value="${voice.name}">${voice.name}</option>`).join('');
+function changeVoice(e){
+  voices = this.getVoices();
+  let options = voices.map(voice => `<option value="${voice.name}">${voice.name}</option>`).join('');
 
   dropdown.innerHTML = options;
 }
 
 function dropdownOptionChange(e){
-  isClosed = 0;
   console.log(this.options[this.selectedIndex].value);
+  speech.voice = voices.find(voice => voice.name === this.options[this.selectedIndex].value);
+  toggleVoice(true);
 }
 
-function changeSpanValue(e){
+function rangeInput(e){
   document.querySelector(`span[data-for=${this.name}]`).innerHTML = Math.trunc(this.value * parseFloat(this.dataset.multiplier));
+  speech[this.name] = this.value;
+  toggleVoice(true);
 }
 
-speech.voice = voices[5];
+function buttonClick(e){
+  speechSynthesis[this.dataset.action]();
+}
+
+function toggleVoice(startOver = false){
+  speechSynthesis.cancel();
+  if (startOver)
+    speechSynthesis.speak(speech);
+}
+
+function textareaKeydown(e){
+  clearTimeout(timer);
+  timer = setTimeout(() => {
+    speech.text = this.value;
+    toggleVoice(true);
+  }, 1500);
+}
+
 speech.voiceURI = 'native';
 speech.volume = 1;
 speech.rate = 1;
@@ -32,7 +55,11 @@ speech.pitch = 1;
 speech.text = str;
 speech.lang = 'en-GB';
 
+speechSynthesis.speak(speech);
 speech.addEventListener('end', e => console.log('Time taken: %s', e.elapsedTime));
 speechSynthesis.addEventListener('voiceschanged', changeVoice);
 dropdown.addEventListener('change', dropdownOptionChange);
-inputRanges.forEach(input => input.addEventListener('input', changeSpanValue));
+inputRanges.forEach(input => input.addEventListener('input', rangeInput));
+buttons[0].addEventListener('click', toggleVoice);
+buttons[1].addEventListener('click', toggleVoice.bind(null, false));
+textarea.addEventListener('keydown', textareaKeydown);
