41627a6ee5807f4a51e3b08669bddfa4485a8028
MarcusGitAccount <pop_marcus_98@yahoo.com>
2017-01-03 17:38:40 +0200

    day23 ui

--- Day 23/js/functions.js
+++ Day 23/js/functions.js
@@ -2,6 +2,8 @@
 if (!('speechSynthesis' in window))
   alert('Your browser does not support speechSynthesis');
 
+let isClosed = 0;
+let inputRanges = document.querySelectorAll('input[type=range]');
 let dropdown = document.querySelector('select[name=voices]');
 let speech = new SpeechSynthesisUtterance();
 let voices = Array.from(window.speechSynthesis.getVoices());
@@ -14,7 +16,12 @@ function changeVoice(e){
 }
 
 function dropdownOptionChange(e){
+  isClosed = 0;
   console.log(this.options[this.selectedIndex].value);
+}
+
+function changeSpanValue(e){
+  document.querySelector(`span[data-for=${this.name}]`).innerHTML = Math.trunc(this.value * parseFloat(this.dataset.multiplier));
 }
 
 speech.voice = voices[5];
@@ -27,5 +34,5 @@ speech.lang = 'en-GB';
 
 speech.addEventListener('end', e => console.log('Time taken: %s', e.elapsedTime));
 speechSynthesis.addEventListener('voiceschanged', changeVoice);
-speechSynthesis.speak(speech);
 dropdown.addEventListener('change', dropdownOptionChange);
+inputRanges.forEach(input => input.addEventListener('input', changeSpanValue));
