0ce1922893ad94aee40ca3e0b0b52ab2ef7a0cdf
MarcusGitAccount <pop_marcus_98@yahoo.com>
2017-01-16 22:11:33 +0200

    day 30

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 
-var https   = require('https');
-var fs      = require('fs');
-var express = require('express');
-var server  = express();
-var folders = {
+'use strict';
+// node --harmony server.js
+
+let https   = require('https');
+let fs      = require('fs');
+let express = require('express');
+let server  = express();
+let folders = {
   '/Day19': 'Day 19',
   '/Day20': 'Day 20',
   '/Day21': 'Day 21'
@@ -16,11 +19,11 @@ server.get('/', function (request, response) {
   response.end('<h1>Root folder for JS-30</h1>');
 });
 
-var httpsServer = https.createServer({
+let httpsServer = https.createServer({
   'key': fs.readFileSync('SSL/key.pem').toString(),
   'cert': fs.readFileSync('SSL/cert.pem').toString()
 }, server);
 
-var listener = httpsServer.listen(8080, '192.168.1.5' || '127.0.0.1', function(){
+let listener = httpsServer.listen(8080, '192.168.1.5' || '127.0.0.1', function(){
   console.log('Listening on https://%s:%s ...', listener.address().address, listener.address().port);
 });
