fe95a99da82a3b9986e906f6038356c06990e203
MarcusGitAccount <pop_marcus_98@yahoo.com>
2017-01-17 14:40:38 +0200

    day21 update

--- Day 21/js/functions.js
+++ Day 21/js/functions.js
@@ -1,12 +1,7 @@
 
-function watchPositionSucces(data){
-  document.body.innerHTML = JSON.stringify(data) + data.timestamp;
-}
 
-function watchPositionError(err){
-  console.error(err);
-}
-
-setInterval(function(){
-  navigator.geolocation.watchPosition(watchPositionSucces, watchPositionError);
-}, 1000);
+navigator.geolocation.watchPosition((data) => {
+  console.log('Speed: %s', data.coords.speed);
+  console.log('Heading: %s', data.coords.heading);
+  console.log('Lat, long: %s %s', data.coords.latitude, data.coords.longitude);
+}, (err) => { console.log('Something bad happened') });
